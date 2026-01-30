5b87442be80e40750eb3482f7f404964ad6f1829
lenakononchuk <lena@rockingtech.co.uk>
2026-01-30 20:31:30 +0200

    Fix tasks display, drag-and-drop, clean up mock data, update README

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/FlowBoard-New/',
+  base: '/FlowBoard/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
